9bd8f3ad2e54578ce98e1e85caf4def34c07e732
sogrey <suzhaoheng2006@163.com>
2018-08-29 11:36:40 +0800

    add rxpermissionsapp

--- RxPermissionsApp/src/main/java/org/sogrey/rxpermissionsapp/MainActivity.java
+++ RxPermissionsApp/src/main/java/org/sogrey/rxpermissionsapp/MainActivity.java
@@ -27,32 +27,73 @@ public class MainActivity extends FragmentActivity {
 
     private void getPermissions() {
         RxPermissions rxPermissions = new RxPermissions(MainActivity.this);
+//        rxPermissions
+//                .request(Manifest.permission.CALL_PHONE, Manifest.permission.CAMERA)
+//                .subscribe(granted -> {
+//                    if (granted) {
+//                        // All requested permissions are granted
+//                        Toast.makeText(MainActivity.this, "同意权限", Toast.LENGTH_SHORT).show();
+//                    } else {
+//                        // At least one permission is denied
+//                        Toast.makeText(MainActivity.this, "至少有一个权限被拒绝", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
         rxPermissions
-                .request(Manifest.permission.CALL_PHONE,Manifest.permission.CAMERA)
-                .subscribe(new Observer<Boolean>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(Boolean value) {
-                        if (value) {
-                            Toast.makeText(MainActivity.this, "同意权限", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(MainActivity.this, "拒绝权限", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-
+                .requestEach(Manifest.permission.CALL_PHONE, Manifest.permission.CAMERA)
+                .subscribe(permission -> { // will emit 2 Permission objects
+                    if (permission.granted) {
+                        // `permission.name` is granted !
+                        Toast.makeText(MainActivity.this, "同意权限:"+permission.name, Toast.LENGTH_SHORT).show();
+                    } else if (permission.shouldShowRequestPermissionRationale) {
+                        // Denied permission without ask never again
+                        Toast.makeText(MainActivity.this, "拒绝不允许再也不允许", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Denied permission with ask never again
+                        // Need to go to the settings
+                        Toast.makeText(MainActivity.this, "拒绝同意再询问，需要去设置", Toast.LENGTH_SHORT).show();
                     }
                 });
+//        rxPermissions
+//                .requestEachCombined(Manifest.permission.CALL_PHONE, Manifest.permission.CAMERA)
+//                .subscribe(permission -> { // will emit 1 Permission object
+//                    if (permission.granted) {
+//                        // All permissions are granted !
+//                        Toast.makeText(MainActivity.this, "同意权限:"+permission.name, Toast.LENGTH_SHORT).show();
+//                    } else if (permission.shouldShowRequestPermissionRationale) {
+//                        // At least one denied permission without ask never again
+//                        Toast.makeText(MainActivity.this, "至少有一个拒绝不允许再也不允许", Toast.LENGTH_SHORT).show();
+//                    } else {
+//                        // At least one denied permission with ask never again
+//                        // Need to go to the settings
+//                        Toast.makeText(MainActivity.this, "至少有一个拒绝同意，要求再也不要，需要去设置", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//        rxPermissions
+//                .request(Manifest.permission.CALL_PHONE, Manifest.permission.CAMERA)
+//                .subscribe(new Observer<Boolean>() {
+//                    @Override
+//                    public void onSubscribe(Disposable d) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onNext(Boolean value) {
+//                        if (value) {
+//                            Toast.makeText(MainActivity.this, "同意权限", Toast.LENGTH_SHORT).show();
+//                        } else {
+//                            Toast.makeText(MainActivity.this, "拒绝权限", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//
+//                    }
+//                });
     }
 }
